654a4e50739e80ad9209b490d50185b7bd7d534e
Lim Victor <lim.victor@mindvalley.com>
2014-01-16 15:20:01 +0800

    if #determine_stack can be called by FuelSDK::Targeting, then the method should not be protected. Move it into public

--- lib/fuelsdk/targeting.rb
+++ lib/fuelsdk/targeting.rb
@@ -11,12 +11,11 @@ module FuelSDK::Targeting
     @endpoint
   end
 
-  protected
-    def determine_stack
-      options = {'params' => {'access_token' => self.access_token}}
-      response = get("https://www.exacttargetapis.com/platform/v1/endpoints/soap", options)
-      @endpoint = response['url']
-    rescue => e
-      raise 'Unable to determine stack using: ' + e.message
-    end
+  def determine_stack
+    options = {'params' => {'access_token' => self.access_token}}
+    response = get("https://www.exacttargetapis.com/platform/v1/endpoints/soap", options)
+    @endpoint = response['url']
+  rescue => e
+    raise 'Unable to determine stack using: ' + e.message
+  end
 end
